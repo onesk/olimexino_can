97cd1b357aa6e9b19e00d206b9ec597632dbb831
Nikita Lesnikov <one.devl@gmail.com>
2016-05-16 03:10:15 +0300

    responsibility redirect

--- transceiver/Src/stm32f1xx_hal_msp.c
+++ transceiver/Src/stm32f1xx_hal_msp.c
@@ -82,9 +82,14 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
   
     /**SPI1 GPIO Configuration    
     PA5     ------> SPI1_SCK
-    PA7     ------> SPI1_MOSI 
+    PA6     ------> SPI1_MISO 
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_7;
+    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_6;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -109,9 +114,9 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
   
     /**SPI1 GPIO Configuration    
     PA5     ------> SPI1_SCK
-    PA7     ------> SPI1_MOSI 
+    PA6     ------> SPI1_MISO 
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5|GPIO_PIN_7);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5|GPIO_PIN_6);
 
   }
   /* USER CODE BEGIN SPI1_MspDeInit 1 */
